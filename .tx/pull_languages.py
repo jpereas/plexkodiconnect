183807c755ae658729cf208eda62c7435b65b694
tomkat83 <kathriner.thomas@gmail.com>
2017-07-25 20:13:57 +0200

    Update translations from transifex

---
+++ .tx/pull_languages.py
@@ -0,0 +1,29 @@
+import os
+
+languages = [
+    'nl_NL',
+    'fr_CA',
+    'fr_FR',
+    'de_DE',
+    'pt_PT',
+    'pt_BR',
+    'es_ES',
+    'es_AR',
+    'es_MX',
+    'cs_CZ',
+    'zh_CN',
+    'zh_TW',
+    'da_DK',
+    'it_IT',
+    'no_NO',
+    'el_GR',
+    'pl_PL',
+    # 'sv_SE',
+    # 'hu_HU',
+    # 'ru_RU',
+]
+
+os.system("cd ..")
+
+for lang in languages:
+    os.system("tx pull -f -l %s" % lang)
